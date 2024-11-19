f6b004224433a27e56a16dc2d089c3e4ff47d162
Viv Hayles <vivhayles@gmail.com>
2024-11-19 11:19:06 +0000

    Added php to html, added PHP code to get information about all projects

---
+++ ScotchEgg.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'src/Services/DatabaseService.php';
+require_once 'src/Models/ProjectModel.php';
+
+
+
+
+$db = DatabaseService::connect();
+
+$projectModel = new ProjectModel($db);
+
+$projects = $projectModel->getProjects();
+
+echo "<pre>";
+var_dump($projects);

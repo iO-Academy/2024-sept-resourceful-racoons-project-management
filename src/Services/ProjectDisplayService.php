f832f327105c65be0cb693c28ba84235053f7a37
Emy <f.emeric.emy@gmail.com>
2024-11-21 15:31:26 +0100

    Deadline dates added color red DONE

--- src/Services/ProjectDisplayService.php
+++ src/Services/ProjectDisplayService.php
@@ -13,13 +13,18 @@ class ProjectDisplayService
     public static function displayProjects(array $projects): string
     {
         $output = '';
-        foreach ($projects as $project)
-        {
-            $output .= " <a href='project.php' class='hover:underline rounded-lg border p-4 py-6 text-4xl font-bold
-            w-full bg-slate-300'>$project->name</a>";
-            //Put the html inside the foreach loop. Must be $project not $projects otherwise it can't read the array.
+        $today = date('Y-m-d');
+        foreach ($projects as $project) {
+            if ($project->deadline < $today) {
+                $output .= "<a href='project.php' class='hover:underline rounded-lg border border-red-600 p-4 py-6 text-4xl font-bold w-full bg-red-300'>
+                        {$project->name}
+                        </a>";
+            } else {
+                $output .= "<a href='project.php' class='hover:underline rounded-lg border p-4 py-6 text-4xl font-bold w-full bg-slate-300'>
+                        {$project->name}
+                        </a>";
+            }
         }
         return $output;
     }
 }
-

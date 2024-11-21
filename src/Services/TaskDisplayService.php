90830bc44db94906d4610fda02c097a30005d6f6
Ash Coles <ashley@acoles.co.uk>
2024-11-21 14:46:20 +0000

    Refactoring date service

--- src/Services/TaskDisplayService.php
+++ src/Services/TaskDisplayService.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'src/Entities/TaskEntity.php';
+require_once 'src/Services/DateService.php';
 // WORK IN PROGRESS
 //class TaskDisplayService
 //{
@@ -36,28 +37,20 @@ class TaskDisplayService
      */
     public static function displayTasks(array $tasks): string
     {
-        $todaysDate = date('Y-m-d');
         $output = '';
         foreach ($tasks as $task) {
-            if (is_null($task->deadline)) {
+            if (!DateService::isOverdue($task->deadline)) {
                 $output .= "<a class='block border rounded border-slate-600 hover:underline mb-3 p-3 bg-slate-300 text-2xl' href='task.php'>
                                 <h3 class='mb-0 font-bold'>$task->taskname
                                 <span class='bg-teal-400 px-2 rounded text-white font-bold float-right'>$task->estimate</span>
                                 </h3>
                             </a>";
-            } else if ($todaysDate > $task->deadline) { 
-                // writing blue($todaysDate > $task->deadline) //needs fixing but displays okay
+            } else {
                 $output .= "<a class='block border rounded border-red-600 hover:underline mb-3 p-3 bg-red-200 border-red-600 text-2xl' href='task.html'>
                                     <h3 class='mb-0 text-red-800 font-bold'>$task->taskname
                                     <span class='bg-teal-400 px-2 rounded text-white font-bold float-right'>$task->estimate</span>
                                 </h3>
                             </a>";
-            } else {
-                $output .= "<a class='block border rounded border-slate-600 hover:underline mb-3 p-3 bg-slate-300 text-2xl' href='task.php'>
-                            <h3 class='mb-0 font-bold'>$task->taskname
-                            <span class='bg-teal-400 px-2 rounded text-white font-bold float-right'>$task->estimate</span>
-                        </h3>
-                    </a>";
             }
         }
         return $output;

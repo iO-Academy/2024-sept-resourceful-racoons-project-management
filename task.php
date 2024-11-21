72de8388299a0abd9abf874f75c6fc72da634ba2
Emy <f.emeric.emy@gmail.com>
2024-11-21 16:33:16 +0100

    cherry picked DONE and Project Name added-

--- task.php
+++ task.php
@@ -29,7 +29,7 @@ $tasks = $taskModel->getTaskById($task_id);
 </header>
 <main class="p-3">
     <div class="flex justify-between mb-3">
-        <h2 class="text-4xl font-bold mb-2">Task Name - 30/06/2024
+        <h2 class="text-4xl font-bold mb-2"><?php echo $tasks->name . ' - ' . $tasks->deadline ?>
             <a href="project.php" class="text-base text-blue-600 hover:underline ms-3">Return to project</a>
         </h2>
         <div class="flex items-center gap-3">

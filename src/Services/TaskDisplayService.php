ca9a8d8de08d1dbf72b10352d88830f240341fc7
Michael Day <michaelday1991@hotmail.co.uk>
2024-11-22 11:34:22 +0000

    Joined Task URL links

--- src/Services/TaskDisplayService.php
+++ src/Services/TaskDisplayService.php
@@ -1,29 +1,7 @@
 <?php
 require_once 'src/Entities/TaskEntity.php';
 require_once 'src/Services/DateService.php';
-// WORK IN PROGRESS
-//class TaskDisplayService
-//{
-//    public static function displaySingle(ProjectEntity $projects): string
-//    {
-//        return "<div>$projects->name</div>";
-//    }
-//
-//
-//    /**
-//     * @param ProjectEntity[] $projects
-//     */
-//    public static function displayProjects(array $projects): string
-//    {
-//        $output = '';
-//        foreach ($projects as $project)
-//        {
-//            $output .= " <a href='project.html' class='hover:underline rounded-lg border p-4 py-6 text-4xl font-bold
-//            w-full md:w-1/4 bg-slate-300'>$project->name</a>";
-//            //Put the html inside the foreach loop. Must be $project not $projects otherwise it can't read the array.
-//        }
-//        return $output;
-//    }
+require_once 'src/Services/TaskDisplayService.php';
 
 class TaskDisplayService
 {
@@ -40,13 +18,13 @@ class TaskDisplayService
         $output = '';
         foreach ($tasks as $task) {
             if (!DateService::isOverdue($task->deadline)) {
-                $output .= "<a class='block border rounded border-slate-600 hover:underline mb-3 p-3 bg-slate-300 text-2xl' href='task.php'>
+                $output .= "<a class='block border rounded border-slate-600 hover:underline mb-3 p-3 bg-slate-300 text-2xl' href='task.php?task_id={$task->id}'> 
                                 <h3 class='mb-0 font-bold'>$task->taskname
                                 <span class='bg-teal-400 px-2 rounded text-white font-bold float-right'>$task->estimate</span>
                                 </h3>
                             </a>";
             } else {
-                $output .= "<a class='block border rounded border-red-600 hover:underline mb-3 p-3 bg-red-200 border-red-600 text-2xl' href='task.html'>
+                $output .= "<a class='block border rounded border-red-600 hover:underline mb-3 p-3 bg-red-200 border-red-600 text-2xl' href='task.php?task_id={$task->id}'>
                                     <h3 class='mb-0 text-red-800 font-bold'>$task->taskname
                                     <span class='bg-teal-400 px-2 rounded text-white font-bold float-right'>$task->estimate</span>
                                 </h3>
